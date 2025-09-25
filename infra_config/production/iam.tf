d09ca98237357cd844792ab10f85fe30ad733720
Godson Nwaubani <nwaubanigodson2@gmail.com>
2025-09-25 22:50:11 +0100

    modified the production infra config file

--- infra_config/production/iam.tf
+++ infra_config/production/iam.tf
@@ -1,11 +1,31 @@
-# Reference the existing IAM role (created in staging)
-data "aws_iam_role" "ec2_ecr_pull" {
+resource "aws_iam_role" "ec2_ecr_pull" {
   name = "ec2-ecr-pull-role"
 
-  tags = local.common_tags
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
 }
 
-# Reference the existing instance profile
-data "aws_iam_instance_profile" "ecr_profile" {
+resource "aws_iam_role_policy_attachment" "ecr_readonly_attach" {
+  role       = aws_iam_role.ec2_ecr_pull.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+
+resource "aws_iam_instance_profile" "ec2_ecr_profile" {
   name = "ec2-ecr-instance-profile"
+  role = aws_iam_role.ec2_ecr_pull.name
+
+  tags = merge(local.common_tags, {
+    Name = "${var.environment}-tracker-app"
+  })
 }
